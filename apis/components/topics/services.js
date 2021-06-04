70587734b5d4cbe5f848c80e026e64359d6c1e28
ElTioGonza <gonz230300@gmail.com>
2021-06-04 16:34:13 -0300

    Now you are sending with each topic's main comments, the author's name

--- apis/components/topics/services.js
+++ apis/components/topics/services.js
@@ -5,40 +5,50 @@ const { Comments, Users, Topics } = require("../../config/sequelize_database");
 const services = {
 
   toggleFavoriteTopic: async (id, topic) => {
-        let { favorite_topics } = await Users.findOne({ attributes: [ "favorite_topics" ], where: { id }, raw: true });
-        const alreadyHasTopic = favorite_topics.includes(topic);
-        favorite_topics = JSON.parse(favorite_topics);
-  
-        if (alreadyHasTopic) {
-          favorite_topics.splice(favorite_topics.indexOf(topic), 1)
-        } else {
-          favorite_topics.push(topic);
-        }
-        favorite_topics = JSON.stringify(favorite_topics);
-        
-        await Users.update({ favorite_topics }, { where: { id } })
+    let { favorite_topics } = await Users.findOne({ attributes: ["favorite_topics"], where: { id }, raw: true });
+    const alreadyHasTopic = favorite_topics.includes(topic);
+    favorite_topics = JSON.parse(favorite_topics);
+
+    if (alreadyHasTopic) {
+      favorite_topics.splice(favorite_topics.indexOf(topic), 1)
+    } else {
+      favorite_topics.push(topic);
+    }
+    favorite_topics = JSON.stringify(favorite_topics);
+
+    await Users.update({ favorite_topics }, { where: { id } })
   },
 
-  createTopicDB: ( author, subject, intro ) => Topics.create({ author, subject, intro }),
+  createTopicDB: (author, subject, intro) => Topics.create({ author, subject, intro }),
 
-  deleteTopicDB: ( author, id ) => Topics.destroy({ where: { [Op.and]: { author, id } } }),
-  
+  deleteTopicDB: (author, id) => Topics.destroy({ where: { [Op.and]: { author, id } } }),
+
   getTopicData: id => Topics.findAll({ where: { id }, raw: true }),
-  
-  getMainComments: topic => Comments.findAll({ where: { [Op.and]: { topic, parent:null } }, raw: true }),
+
+  getMainComments: async topic => {
+    let comments = await Comments.findAll({ where: { [Op.and]: { topic, parent: null } }, raw: true });
+    const authoredComments = [];
+    for await (const comment of comments) {
+      const data = await Users.findOne({ attributes: ["username"], where: { id: comment.author }, raw: true });
+      const author = data?.username ?? "Anonimous";
+      authoredComments.push({ ...comment, author });
+    }
+    
+    return authoredComments;
+  },
 
   getAllTopicsDB: async () => {
     const topics = await Topics.findAll({ raw: true });
     const topicsWithAuthor = [];
-    for await (const topic of topics){
-      const authorRaw = await Users.findOne({attributes: ["username"], where: { id: topic.author }, raw: true });
+    for await (const topic of topics) {
+      const authorRaw = await Users.findOne({ attributes: ["username"], where: { id: topic.author }, raw: true });
       const author = authorRaw?.username ?? "Anonimous";
       topicsWithAuthor.push({ ...topic, author });
     }
-    
+
     return topicsWithAuthor;
-  } 
-  
+  }
+
 }
 
 
